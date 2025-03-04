552dadc4a2b4f35045c0b49e393ec04c5d676b7e
Jake Felice <jasonfelice0@gmail.com>
2025-03-04 15:27:17 +0530

    Add favicon

--- src/components/Page.js
+++ src/components/Page.js
@@ -6,13 +6,13 @@ export default function Page() {
   return (
     <section className={Styles.main_wrapper}>
         <div className={Styles.logo_wrapper}>
-          <img src={logo} alt="RomeVille Logo" />
-          <span>RomeVille</span>
+          <img className="noselect" draggable={false} src={logo} alt="RomeVille Logo" />
+          <span className="noselect">RomeVille</span>
         </div>
         <div className={Styles.socials}>
             <a href="/#"> <i className={Styles.ig} /> </a>
-            <a href="/#"> <i className={Styles.fb} /> </a>
             <a href="/#"> <i className={Styles.wa} /> </a>
+            <a href="/#"> <i className={Styles.fb} /> </a>
             <a href="/#"> <i className={Styles.mail} /> </a>
         </div>
     </section>
