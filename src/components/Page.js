a26c9602d860ebdfebb74348732757e1e4a2331c
Jake Felice <jasonfelice0@gmail.com>
2025-03-04 15:47:02 +0530

    Animate logo

--- src/components/Page.js
+++ src/components/Page.js
@@ -6,14 +6,14 @@ export default function Page() {
   return (
     <section className={Styles.main_wrapper}>
         <div className={Styles.logo_wrapper}>
-          <img className="noselect" draggable={false} src={logo} alt="RomeVille Logo" />
+          <img className={`${Styles.logo}, noselect`} draggable={false} src={logo} alt="RomeVille Logo" />
           <span className="noselect">RomeVille</span>
         </div>
         <div className={Styles.socials}>
-            <a href="/#"> <i className={Styles.ig} /> </a>
-            <a href="/#"> <i className={Styles.wa} /> </a>
-            <a href="/#"> <i className={Styles.fb} /> </a>
-            <a href="/#"> <i className={Styles.mail} /> </a>
+            <a target="_blank" href="/#"> <i className={Styles.ig} /> </a>
+            <a target="_blank" href="/#"> <i className={Styles.wa} /> </a>
+            <a target="_blank" href="/#"> <i className={Styles.fb} /> </a>
+            <a target="_blank" href="/#"> <i className={Styles.mail} /> </a>
         </div>
     </section>
   )
