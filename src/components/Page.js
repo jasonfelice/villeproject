023fbd94ee1f9eb06ae089063365f1214a6bac07
Jake Felice <jasonfelice0@gmail.com>
2025-03-05 13:43:41 +0530

    Add social links

--- src/components/Page.js
+++ src/components/Page.js
@@ -10,10 +10,10 @@ export default function Page() {
           <p className="noselect">RomeVille</p>
         </div>
         <div className={Styles.socials}>
-            <a target="_blank" href="/#"> <i className={Styles.ig} /> </a>
-            <a target="_blank" href="/#"> <i className={Styles.wa} /> </a>
-            <a target="_blank" href="/#"> <i className={Styles.fb} /> </a>
-            <a target="_blank" href="/#"> <i className={Styles.mail} /> </a>
+            <a target="_blank" rel="noreferrer" href="https://www.instagram.com/romevilleofficial/"> <i className={Styles.ig} /> </a>
+            <a target="_blank" rel="noreferrer" href="https://wa.link/ooea7s"> <i className={Styles.wa} /> </a>
+            <a target="_blank" rel="noreferrer" href="https://www.facebook.com/RomevilleOfficial/"> <i className={Styles.fb} /> </a>
+            <a target="_blank" rel="noreferrer" href="mailto:hello@romeville.com"> <i className={Styles.mail} /> </a>
         </div>
         <footer className={Styles.footer}>
           <p>The website is currently under construction.</p>
